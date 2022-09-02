b06d44cf78c8b02147d99d8f15d0764058c97f74
david tilley <david.ag.tilley@gmail.com>
2022-09-02 12:52:51 +0000

    Added Chapter 8 labs

--- DEVNET-ASC-LABS/6.2.7/sample_app.py
+++ DEVNET-ASC-LABS/6.2.7/sample_app.py
@@ -6,6 +6,24 @@ from flask import render_template
 
 sample = Flask(__name__)
 
+
+@sample.route("/dave")
+def myFunc1():
+    f = open("Func1-app.log", "w")
+    count = 10
+    while (count): 
+      f.write("Dave: Is running {}\n".format(count))
+      count = count - 1
+    f.close()
+    return render_template("dave.html")
+
+@sample.route("/joe")
+def myFunc2():
+    print("Joe: Run this instead")
+    return render_template("joe.html")
+
+
+
 @sample.route("/")
 def main():
     print( "Python: You are calling me from " + request.remote_addr )
--- DEVNET-ASC-LABS/6.2.7/tempdir/sample_app.py
+++ DEVNET-ASC-LABS/6.2.7/tempdir/sample_app.py
@@ -6,13 +6,29 @@ from flask import render_template
 
 sample = Flask(__name__)
 
+
+@sample.route("/dave")
+def myFunc1():
+    f = open("Func1-app.log", "w")
+    count = 10
+    while (count): 
+      f.write("Dave: Is running {}\n".format(count))
+      count = count - 1
+    f.close()
+    return render_template("dave.html")
+
+@sample.route("/joe")
+def myFunc2():
+    print("Joe: Run this instead")
+    return render_template("joe.html")
+
+
+
 @sample.route("/")
 def main():
-    print("Started from Flask")
     print( "Python: You are calling me from " + request.remote_addr )
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print("Started from Python shell")
     sample.run(host="0.0.0.0", port=8080)
 
